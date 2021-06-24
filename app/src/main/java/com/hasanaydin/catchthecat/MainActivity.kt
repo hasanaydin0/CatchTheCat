55191e6cef3bbd5dfb7aba8c9339def1bbcd07ce
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-24 16:32:03 +0300

    ImageVisibility

--- app/src/main/java/com/hasanaydin/catchthecat/MainActivity.kt
+++ app/src/main/java/com/hasanaydin/catchthecat/MainActivity.kt
@@ -3,21 +3,49 @@ package com.hasanaydin.catchthecat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.hasanaydin.catchthecat.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var score = 0
+    val imageArray = ArrayList<ImageView>()
+    var handler = Handler()
+    var runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // ImageArray
+
+        imageArray.add(binding.imageView)
+        imageArray.add(binding.imageView2)
+        imageArray.add(binding.imageView3)
+        imageArray.add(binding.imageView4)
+        imageArray.add(binding.imageView5)
+        imageArray.add(binding.imageView6)
+        imageArray.add(binding.imageView7)
+        imageArray.add(binding.imageView8)
+        imageArray.add(binding.imageView9)
+        imageArray.add(binding.imageView10)
+        imageArray.add(binding.imageView11)
+        imageArray.add(binding.imageView12)
+        imageArray.add(binding.imageView13)
+        imageArray.add(binding.imageView14)
+        imageArray.add(binding.imageView15)
+        imageArray.add(binding.imageView16)
+
+        hideImages()
 
         // CountDownTimer
 
@@ -28,6 +56,12 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 binding.timeText.text = "Time: 0"
+
+                handler.removeCallbacks(runnable)
+
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
 
 
                 // Alert
@@ -48,6 +82,26 @@ class MainActivity : AppCompatActivity() {
             }
 
         }.start()
+    }
+
+    fun hideImages(){
+
+        runnable = object : Runnable{
+            override fun run() {
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = Random()
+                val randomIndex = random.nextInt(16)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+
+        }
+        handler.post(runnable)
+
     }
 
     fun increaseScore(view : View){
