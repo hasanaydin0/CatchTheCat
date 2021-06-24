4bc84f39a33a1ff02a31e9b4d5edfe2138adae74
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-24 15:12:07 +0300

    Increase Score

--- app/src/main/java/com/hasanaydin/catchthecat/MainActivity.kt
+++ app/src/main/java/com/hasanaydin/catchthecat/MainActivity.kt
@@ -2,16 +2,23 @@ package com.hasanaydin.catchthecat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.hasanaydin.catchthecat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    fun increaseScore(view : View){
+        score++
+        binding.scoreText.text = "Score: $score"
     }
 }
